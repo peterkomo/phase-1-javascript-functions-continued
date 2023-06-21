63bd0a7dc7542c0eac773a38376803185c628dfb
peterkomo <mwangipeterkomo@gmail.com>
2023-06-21 05:16:32 +0300

    yet to be done,one error

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  function mondayWork(events="go to the office."){
+    return `This Monday, I will ${events}`
+     }
+     mondayWork('This Monday, I will work from home.')
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("yaass!");
+  
